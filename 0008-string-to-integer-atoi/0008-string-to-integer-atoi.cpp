cafa7a553db52b81dacf9bc96d135ba92dbcc3ca
Ashutosh Kumar Yadav <nasxashu@gmail.com>
2026-08-07 23:03:54 +0530

    Time: 0 ms (100%), Space: 9.1 MB (82.43%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int myAtoi(string s) {
         int n = s.length();
-        int ans = 0;
+        long long ans = 0;
         int i = 0;
         int sign = 1;
         while (i < n && s[i] == ' '){
@@ -19,18 +19,13 @@ public:
             if(isdigit(s[i])) {
                 //convert char into digit
                 int digit = s[i] - '0';
-                // form the digit to a number
-                if(ans > (INT_MAX - digit)/10){
-                    if(sign == 1)
-                    {
-                        return INT_MAX;
-                    }
-                    else
-                    {
-                        return INT_MIN;
-                    }
+                ans = ans * 10 + digit;
+                if(sign*ans < INT_MIN){
+                    return INT_MIN;
                 }
-                    ans = ans * 10 + digit;
+                if(sign*ans > INT_MAX){
+                    return INT_MAX;
+                }
                 i++;
             }
             else{
