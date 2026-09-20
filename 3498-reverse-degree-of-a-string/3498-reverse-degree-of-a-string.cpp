3a829322e619df7ecba4c025fe111c2ab182aa3f
Ashutosh Kumar Yadav <nasxashu@gmail.com>
2026-09-20 19:31:21 +0530

    Time: 1 ms (24.25%), Space: 9.9 MB (8.02%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        vector<char> temp;
-        for(char a = 'z'; a >= 'a'; a--)
-        {
-            temp.push_back(a);
-        }
+        // Without Using Space Solution
+        // O(n)-->time complexity
+        // O(1)--> Space Complexity
+
         int ans = 0;
         for(int i = 1; i <= s.length(); i++)
         {
-            int j = 1;
-            while(j <= 26) {
-                if(temp[j-1] == s[i-1])
-                {
-                    break;
-                }
-                else{
-                    j++;
-                }
-            }
-            ans = ans + j * i;
+            ans = ans + (26 - (s[i-1] - 'a')) * (i);
         }
         return ans;
     }
